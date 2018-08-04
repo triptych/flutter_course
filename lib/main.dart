e2829aa8718c3b423362af8d30e9d0caa6ca0d12
Andrew Wooldridge <awooldridge@ebay.com>
2018-08-03 17:08:04 -0700

    more tutorial changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
+class MyApp extends StatelessWidget {
+  
 
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,32 +17,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('Advanced Food Tester');
-                  });
-                },
-                child: Text('Add Product'),
-              ),
-            ),
-            Column(
-                children: _products
-                    .map((element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset('assets/food.jpg'),
-                              Text(element)
-                            ],
-                          ),
-                        ))
-                    .toList())
-          ],
-        ),
+        body:ProductManager(),
       ),
     );
   }
